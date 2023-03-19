5c20e3e5f4d61e8695902910c649ad55751ef0b0
iequalsone <johnny.rtn@gmail.com>
2023-03-19 08:34:25 -0230

    init git

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float reloaderDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+    bool hasCrashed = false;
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "ground" && !hasCrashed)
+        {
+            hasCrashed = true;
+            FindObjectOfType<PlayerController>().DisableControls();
+            crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
+            Invoke("ReloadScene", reloaderDelay);
+        } 
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/DustTrail.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustTrail : MonoBehaviour
+{
+    [SerializeField] ParticleSystem dustTrailEffect;
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "ground")
+        {
+            dustTrailEffect.Play();
+        } 
+    }
+
+    void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.tag == "ground")
+        {
+            dustTrailEffect.Stop();
+        }
+    }
+}
